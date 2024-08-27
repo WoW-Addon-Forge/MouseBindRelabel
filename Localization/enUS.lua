ed01a1c524721a70390b223b4df028ef11dad3b3
serious <seriouz@gmail.com>
2024-08-27 22:23:23 +0200

    fix: "sMW U" gets truncated in the english client - switched to "sMw U" to prevent this

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -3,8 +3,8 @@ if KeyBindingReplacements ~= nil then return end
 KeyBindingReplacements = {
     ["^Mouse Wheel Up"] = "MW U",
     ["^Mouse Wheel Down"] = "MW D",
-    ["([sac])%-Mouse Wheel Up"] = "%1MW U",
-    ["([sac])%-Mouse Wheel Down"] = "%1MW D",
+    ["([sac])%-Mouse Wheel Up"] = "%1Mw-U",
+    ["([sac])%-Mouse Wheel Down"] = "%1Mw D",
     ["Middle Mouse"] = "MW",
     ["Mouse Button"] = "M"
 }
